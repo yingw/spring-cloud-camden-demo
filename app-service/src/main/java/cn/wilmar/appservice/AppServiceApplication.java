b95c289f24f7791c8bf0b3f5d8e7fb7932314a3d
yingw <yinguowei@gmail.com>
2016-11-14 18:03:18 +0800

    New changelist

--- app-service/src/main/java/cn/wilmar/appservice/AppServiceApplication.java
+++ app-service/src/main/java/cn/wilmar/appservice/AppServiceApplication.java
@@ -1,12 +1,17 @@
 package cn.wilmar.appservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,7 +20,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Stream;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AppServiceApplication {
 
@@ -95,5 +100,21 @@ class UserDataCLR implements CommandLineRunner {
     public void run(String... strings) throws Exception {
         Stream.of("Gary", "Will", "James", "Jon").forEach(name -> userRepository.save(new User(name)));
         userRepository.findAll().forEach(System.out::println);
+    }
+}
+
+@RestController
+@RefreshScope
+class MessageController {
+    private final String message;
+
+    @Autowired
+    public MessageController(@Value("${message}") String message) {
+        this.message = message;
+    }
+
+    @GetMapping("/message")
+    public String say() {
+        return message;
     }
 }
